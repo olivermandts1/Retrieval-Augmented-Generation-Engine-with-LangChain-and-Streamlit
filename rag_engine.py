b82332dbd49c64f86491081149a74a1d2ed77a48
olivermandts1 <104032123+olivermandts1@users.noreply.github.com>
2024-02-12 21:24:28 +0000

    forgot to add sidebar front end option

--- rag_engine.py
+++ rag_engine.py
@@ -4,7 +4,7 @@ import prompt_chain_builder
 import creative_text_refresher
 
 st.sidebar.title("Navigation")
-choice = st.sidebar.radio("Choose a menu", ("RAG Testing Form", "Google Sheets Import"))
+choice = st.sidebar.radio("Choose a menu", ("RAG Testing Form", "Google Sheets Import", 'Creative Text Refresher'))
 
 if choice == "RAG Testing Form":
     rag_testing.show_rag_testing_form()
