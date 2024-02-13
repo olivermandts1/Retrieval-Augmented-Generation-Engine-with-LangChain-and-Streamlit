dd89c9dd15d36b206f198974bf0eace87d7f1737
olivermandts1 <104032123+olivermandts1@users.noreply.github.com>
2024-02-13 17:23:11 +0000

    updating openai secret placement in rag testing

--- rag_testing.py
+++ rag_testing.py
@@ -26,7 +26,6 @@ def show_rag_testing_form():
     if 'retriever' not in st.session_state:
         st.session_state.retriever = None
 
-    st.session_state.openai_api_key = st.secrets["openai_secret"]
     st.session_state.source_docs = st.file_uploader(label="Upload Documents", type="txt", accept_multiple_files=True)
 
     def load_and_split_documents():
@@ -50,7 +49,7 @@ def show_rag_testing_form():
 
     def query_llm(retriever, query):
         qa_chain = ConversationalRetrievalChain.from_llm(
-            llm=OpenAIChat(openai_api_key=st.session_state.openai_api_key),
+            llm=OpenAIChat(openai_api_key=st.secrets["openai_secret"]),
             retriever=retriever,
             return_source_documents=True,
         )
